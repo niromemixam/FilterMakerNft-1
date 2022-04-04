852c1444aaef5239053af26a66fc3cb38a042595
Maxime Morin <maximemorin@MacBook-Pro-de-Maxime.local>
2022-04-04 19:53:28 +0200

    Change webpack.config

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -8,6 +8,24 @@ if (document.title === '') {
   document.title = CollectionConfig.tokenName;
 }
 
+const repo = "/filtermaker-nft-1/"
+
+const favIcon = document.getElementById('favIcon')
+const css = document.getElementById('css')
+const js = document.getElementById('js')
+
+// if(process.env.NODE_ENV === "production"){
+//   favIcon?.setAttribute('href', `${repo}/build/images/fav.png`)
+//   css?.setAttribute('href', `${repo}/build/main.css`)
+//   js?.setAttribute('src', `${repo}/build/main.js`)
+// }else{
+//   favIcon?.setAttribute('href', '/build/images/fav.png')
+//   css?.setAttribute('href', '/build/main.css')
+//   js?.setAttribute('src', '/build/main.js')
+// }
+  
+
+
 document.addEventListener('DOMContentLoaded', async () => {
   ReactDOM.render(<Dapp />, document.getElementById('minting-dapp'));
 });
