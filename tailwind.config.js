430b153d2a8f585f33a8185ec63892924fbe92fd
Maxime Morin <maximemorin@MacBook-Pro-de-Maxime.local>
2022-04-04 14:19:26 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+    mode: 'jit',
+    content: [
+        './src/**/*.tsx',
+        './public/index.html',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                popupsbg: colors.white,
+                neutral: colors.slate,
+                primary: colors.indigo,
+                primarytxt: colors.white,
+                warning: colors.yellow,
+                warningtxt: colors.black,
+                error: colors.red,
+                errortxt: colors.white,
+                filter: '#00293b'
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
